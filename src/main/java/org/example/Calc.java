55096aedf87a14eec17936d4fcc23cc204a0ee20
bw1611 <bw1611@naver.com>
2023-03-10 17:02:45 +0900

    Tdd * 추가

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -3,17 +3,33 @@ package org.example;
 public class Calc {
 
     public static int run(String str){
+        boolean needToMultiple = str.contains("*");
+        boolean needToPlus = !needToMultiple;
 
-        str = str.replaceAll("- ", "+ -");
+        if (needToPlus) {
+            str = str.replaceAll("- ", "+ -");
 
-        String[] bits = str.split(" \\+ ");
+            String[] bits = str.split(" \\+ ");
 
-        int sum = 0;
+            int sum = 0;
 
-        for (int i = 0; i < bits.length; i++){
-            sum += Integer.parseInt(bits[i]);
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+        else if (needToMultiple){
+            String[] bits = str.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++){
+                sum *= Integer.parseInt(bits[i]);
+            }
+            return sum;
         }
 
-        return sum;
+        throw new RuntimeException("올바른 식이 아닙니다.");
     }
 }
--- src/test/java/org/example/CalcTests.java
+++ src/test/java/org/example/CalcTests.java
@@ -67,4 +67,16 @@ public class CalcTests {
     void t10(){
         assertThat(Calc.run("10 - 10 - 10 - 10 - 10 - 10 + 10")).isEqualTo(-30);
     }
+
+    @Test
+    @DisplayName("10 * 10 = 100")
+    void t11(){
+        assertThat(Calc.run("10 * 10")).isEqualTo(100);
+    }
+
+    @Test
+    @DisplayName("10 * 10 * 10 = 1000")
+    void t12(){
+        assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
+    }
 }
